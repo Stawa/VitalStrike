f2732ddcddedfbf4618499a9259ba1808c76be46
Stawa <stawamusic@gmail.com>
2025-01-31 06:20:27 +0800

    Update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,5 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -9,16 +11,25 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/VitalStrike/" : "/",
+  base: "/VitalStrike/",
   plugins: [
     remix({
       buildDirectory: "dist",
+      basename: "/VitalStrike/",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+      },
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
       },
     }),
     tsconfigPaths(),
